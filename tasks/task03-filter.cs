c7a502d487fbf98e36f8b4f0889e2652e9b4ece3
dashakunica <dashka_kunickaya@mail.ru>
2019-11-21 23:13:24 +0300

    add tasks solutions

---
+++ tasks/task03-filter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// Задача: реализовать метод Filter, который должен возвращать входной массив, в котором удалены элементы, которые встречаются более одного раза.
+//   * Значения в массиве должны следовать в том порядке, в котором они следуют в оригинальном массиве.
+//   * Если в массиве встречаются повторяющиеся значения, то они ВСЕ значения должны быть удалены.
+//   * Метод должен выбрасывать исключение ArgumentNullException в случае, если в метод передали null.
+//   * В решении разрешается использовать только конструкции языка. Использовать LINQ запрещено.
+
+public class Program
+{
+    public static int[] Filter(int[] source)
+    {
+
+        if (source == null)
+        {
+            throw new ArgumentNullException("Please send an appropriate array.");
+        }
+
+        int length = source.Length;
+        int numDuplicates = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            int currentElement = source[i];
+            bool isDuplicate = false;
+
+            for (int j = i + 1; j < length - numDuplicates; j++)
+            {
+                // Moving all copies of currentElement at the end of array
+                if (source[j] == currentElement)
+                {
+                    for (int k = j; k < length - numDuplicates - 1; k++)
+                    {
+                        source[k] = source[k + 1];
+                    }
+                    source[length - numDuplicates - 1] = currentElement;
+                    numDuplicates++;
+
+                    isDuplicate = true;
+                }
+            }
+
+            // Removing current Element at the end of array
+            if (isDuplicate)
+            {
+                for (int k = i; k < length - numDuplicates - 1; k++)
+                {
+                    source[k] = source[k + 1];
+                }
+                source[length - numDuplicates - 1] = currentElement;
+                numDuplicates++;
+
+                isDuplicate = false;
+
+                // Prevent switching at next index because of moving
+                // currentElement at the end and replacing him at the next nubmer
+                i--;
+            }
+        }
+
+        Array.Resize<int>(ref source, length - numDuplicates);
+
+        return source;
+    }
+
+
+    public static void Main()
+    {
+        Console.WriteLine("Task is done when all test cases are correct:");
+
+        int testCaseNumber = 1;
+
+        TestReturnedValues(testCaseNumber++, new int[] { }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0 }, new int[] { 0 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1 }, new int[] { 0, 1 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 0 }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0 }, new int[] { 1 });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 0, 1 }, new int[] { });
+        TestReturnedValues(testCaseNumber++, new int[] { 0, 1, 2, 2, 5, 4, 4, 5, 1, 8, 4, 9, 1, 3, 4, 5, 7 }, new int[] { 0, 8, 9, 3, 7 });
+        TestException<ArgumentNullException>(testCaseNumber++, null);
+
+        if (correctTestCaseAmount == testCaseNumber - 1)
+        {
+            Console.WriteLine("Task is done.");
+        }
+        else
+        {
+            Console.WriteLine("TASK IS NOT DONE.");
+        }
+    }
+
+    private static void TestReturnedValues(int testCaseNumber, int[] array, int[] expectedResult)
+    {
+        try
+        {
+            var result = Filter(array);
+
+            if (result.SequenceEqual(expectedResult))
+            {
+                Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                correctTestCaseAmount++;
+            }
+            else
+            {
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+    }
+
+    private static void TestException<T>(int testCaseNumber, int[] array) where T : Exception
+    {
+        try
+        {
+            Filter(array);
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+        catch (T)
+        {
+            Console.WriteLine(correctCaseTemplate, testCaseNumber);
+            correctTestCaseAmount++;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+    }
+
+    private static string correctCaseTemplate = "Case #{0} is correct.";
+    private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
+    private static int correctTestCaseAmount = 0;
+}
